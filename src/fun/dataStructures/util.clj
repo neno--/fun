1ec802569a1b7a373b097a30154f8a023ace1a7b
nenad <neno.nenadovic@gmail.com>
2020-06-19 14:50:47 +0200

    add util, list vs vector, vector vs vec

---
+++ src/fun/dataStructures/util.clj
@@ -0,0 +1,10 @@
+(ns fun.dataStructures.util)
+
+; COUNT
+(println (count '(1 2 3)))
+
+; RANGE
+(println (range 5))
+
+
+
--- src/fun/dataStructures/vectors.clj
+++ src/fun/dataStructures/vectors.clj
@@ -8,9 +8,15 @@
 (println (get [1 2 3 4] 0))
 
 ; can be counted
-
 (println "count:" (count [1 2 3]))
 
 ; adding elements
-
 (println (conj [1 2 3 4] 5 6 7))
+
+; vector vs list vs CONJ
+(println (conj [1 2 3] 4))
+(println (conj '(1 2 3) 4))
+
+; vector as a single argument
+(println (vector (range 3)))
+(println (vec (range 3)))
