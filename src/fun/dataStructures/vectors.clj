5079eaba798be01df145dbcb76b699cf23b8d2d5
nenad <neno.nenadovic@gmail.com>
2020-06-22 20:10:34 +0200

    find max

--- src/fun/dataStructures/vectors.clj
+++ src/fun/dataStructures/vectors.clj
@@ -20,3 +20,22 @@
 ; vector as a single argument
 (println (vector (range 3)))
 (println (vec (range 3)))
+
+; first and rest works
+(println (first [1 2 3]))
+(println (rest [1 2 3]))
+
+; find max element in vector with reduce
+(defn find-max [elements]
+  (reduce (fn [first second]
+            (if (> first second) first second)) elements))
+
+(println (find-max [1 3 6 3 4 10 2]))
+
+; find max element with loop
+(defn find-max-loop [_elements]
+  ((fn [[first & rest] max]
+     (if (empty? rest) (if (> first max) first max) (if (> first max) (recur rest first) (recur rest max))))
+   (rest _elements) (first _elements)))
+
+(println (find-max-loop [1 3 6 3 4 10 2]))
