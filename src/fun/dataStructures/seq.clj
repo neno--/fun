8e33221b7a6230eb73c774daf81ad3f48d4f9856
nenad <neno.nenadovic@gmail.com>
2020-06-25 08:45:34 +0200

    more functions for ISeq

--- src/fun/dataStructures/seq.clj
+++ src/fun/dataStructures/seq.clj
@@ -16,3 +16,28 @@
 ; first and rest functions call seq on their arguments, for performance reasons
 (println (type (rest '(1 2 3))))
 (println (type (rest [1 2 3])))
+
+; CONS - a list like data structure
+; holds a value and references another sequence ISeq
+
+; next, like rest, but at the end returns NIL an not an empty sequence
+(println (rest []))
+(println (next []))
+
+; nth like get, but throws out of bounds exception or you can supply a default value
+(println (get [1 2 3] 3))
+(println (nth [1 2 3] 3 "java.lang.IndexOutOfBoundsException"))
+
+; apply
+(apply println "hello" "world" ["from" "clojure"])
+
+; map - maps until shortest is exhausted - returns LAZY sequence
+(println (map + [1 2 3] [10 10]))
+
+; reduce & reductions (intermediate results)
+(println (reduce + [1 2 3]))
+(println (reductions + [1 2 3]))
+
+; filter and remove
+(println (filter (fn [dont_care] true) [1 2 3]))
+(println (remove (fn [dont_care] false) [1 2 3]))
